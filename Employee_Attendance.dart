c08451d838316c11e9290170789bc3790d22efd9
Nikita khapare <nikitakhapare53@gmail.com>
2022-06-23 21:49:43 +0530

    [Nikita]Add:Employee Wage Program using dart

--- Employee_Attendance.dart
+++ Employee_Attendance.dart
@@ -1,19 +1,25 @@
 import 'dart:math';
 
 class Employee {
-  void attendance() {
+  int attendance() {
+    int wagePerHour = 20;
+    int workingHour = 8;
+    int dailyWage = 0;
     var rand = new Random();
     var checker = rand.nextInt(2);
     if (checker == 1) {
       print("Employee is present");
+      dailyWage = wagePerHour * workingHour;
     } else {
       print("Employee is absent");
     }
+    return dailyWage;
   }
 }
 
 void main() {
   var emp = new Employee();
   print("Welcome To employee wage Computation program");
-  emp.attendance();
+  print("Employee wage is ${emp.attendance()}");
 }
+
